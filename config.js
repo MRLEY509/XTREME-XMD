199a369d44b71ad9dc8c0573d3c16272650531fc
PRINCE XTREME <ley376171@gmail.com>
2025-07-29 19:49:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~TtoTEIwT#Rkrx3lAfXSWU6NXRkBV-qLgCokqa8GQMDqwWXDXCVnA",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~L4RS2SjR#dWZQVVVWlbW68TrwxG7s6baazTAPife9f2u8wsUuoo4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "528145550802",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50941319791",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ᴘʀɪɴᴄᴇ xᴛʀᴇᴍᴇ",
 // add bot owner name
